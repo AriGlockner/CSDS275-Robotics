6ddb383140b5ece1de2a5412646ccf7d90343603
Ari Glockner <ari@glockners.net>
2023-11-09 14:20:28 -0500

    Finished HW5 Part 1 (hopefully)

--- Homework/HW5/my_functions.py
+++ Homework/HW5/my_functions.py
@@ -13,8 +13,9 @@ import copy
 
 '''------------------ COMPLETE THE FUNCTIONS BELOW    --------------------------'''
 
+
 def threshold(image_array,thresh=25):
-    '''
+    """
     PART 1 QUESTION 3A
     Apply a threshold to a grayscale image array to identify pixel positions exceeding a specified threshold.
 
@@ -25,53 +26,60 @@ def threshold(image_array,thresh=25):
     Returns:
     tuple: A tuple containing two NumPy arrays. The first array represents the y-positions (rows) of pixels exceeding the threshold,
         and the second array represents the x-positions (columns) of pixels exceeding the threshold.
-    
-    '''
 
-    # hint the np.where function can be useful
-    
-    return pixel_positions
+    """
+    return np.where(image_array >= thresh)
 
 
 def get_pixel_centroid(pixel_positions):
-    '''
+    """
     PART 1 QUESTION 3B
     Calculate the centroid (center of mass) of a set of pixels in an image based on their positions.
 
-    Args:
-        pixel_positions (numpy.ndarray): A 2 element list with numpy vectors of length N representing the positions of pixels in an image.
-        The first element contains the y-positions (rows) of the pixels, and the second row contains the x-positions (columns) of the pixels.
+    Args: pixel_positions (numpy.ndarray): A 2 element list with numpy vectors of length N representing the positions
+    of pixels in an image. The first element contains the y-positions (rows) of the pixels, and the second row
+    contains the x-positions (columns) of the pixels.
 
     Returns:
     numpy.ndarray: A 1D NumPy array of shape (2,) representing the centroid coordinates of the pixel positions.
         The first element of the array (centroid[0]) corresponds to the x-coordinate (u-bar),
-        and the second element (centroid[1]) corresponds to the y-coordinate (v-bar) of the centroid.'''
-    
-    
-    return np.array((u_bar,v_bar))
+        and the second element (centroid[1]) corresponds to the y-coordinate (v-bar) of the centroid.
+    """
+    return np.array((np.mean(pixel_positions[1]), np.mean(pixel_positions[0])))
 
 
-def compute_pos_from_pix(pixel_uv,resolution,focal_length,pixels_per_inch,z_distance):
-    '''
+def compute_pos_from_pix(pixel_uv, resolution, focal_length, pixels_per_inch, z_distance):
+    """
     PART 1 QUESTION 3C
     Calculate the real-world position coordinates from pixel coordinates in a camera image.
-    
+
     Args:
         pixel_uv (tuple): A tuple representing the pixel coordinates (u, v) in the camera image.
         resolution (int): The resolution of the camera image, typically measured in pixels.
         focal_length (float): The focal length of the camera lens in meters.
         pixels_per_inch (float): The number of pixels per inch of the camera sensor.
         z_distance (float): The distance of the object from the camera along the optical axis, in meters.
-    
-    Returns:
-        numpy.ndarray: A 1D NumPy array of shape (3,) representing the real-world position coordinates (x, y, z) of the object.
-    
-    '''
-    
-    
-    return np.array((x_pos,y_pos,z_distance))
 
-def threshold_RGB(image_array,target_color,thresh=25):
+    Returns: numpy.ndarray: A 1D NumPy array of shape (3,) representing the real-world position coordinates (x, y, z)
+    of the object.
+    """
+
+    # Convert from pixels to image plane coordinates
+    u = pixel_uv[0] - resolution[0] / 2
+    v = pixel_uv[1] - resolution[1] / 2
+
+    # Convert from image plane coordinates to camera coordinates
+    x_pos = u * z_distance / focal_length
+    y_pos = v * z_distance / focal_length
+
+    # Convert from camera coordinates to world coordinates
+    x_pos /= pixels_per_inch
+    y_pos /= pixels_per_inch
+
+    return np.array((x_pos, y_pos, z_distance))
+
+
+def threshold_RGB(image_array, target_color, thresh=25):
     '''
     PART 2 QUESTION 1
     Apply a threshold to an RGB image array to identify pixel positions exceeding a specified threshold in a specific color channel.
